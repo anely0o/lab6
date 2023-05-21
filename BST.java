b0e57ba9ee670042430701fe8275c69b690528d9
anely0o <ramyun.oppar@gmail.com>
2023-05-21 23:50:50 +0600

    Update BST.java

--- BST.java
+++ BST.java
@@ -7,3 +7,18 @@ public class BST<K extends Comparable<K>, V> {
     private class Node {
         private K key;
         private V val;
+          this.key =key;
+            this.val =val;
+    }
+}
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+    // Recursive helper method to insert a key-value pair into the binary search tree
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+    } else if (cmp > 0) {
+        node.right = put(node.right, key, val);
+        } else {
