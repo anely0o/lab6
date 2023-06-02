d94a5c4cab7432bcf682b28670e61ce3bc7686ab
anely0o <ramyun.oppar@gmail.com>
2023-06-02 09:10:30 +0600

    final 6

--- Main.java
+++ Main.java
@@ -1,26 +1,54 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        BST<Integer, String> bst = new BST<>();
 
-        bst.put(5, "Value 5"); // Insert key-value pair (5, "Value 5")
-        bst.put(2, "Value 2"); // Insert key-value pair (2, "Value 2")
-        bst.put(7, "Value 7"); // Insert key-value pair (7, "Value 7")
-        bst.put(1, "Value 1"); // Insert key-value pair (1, "Value 1")
-        bst.put(4, "Value 4"); // Insert key-value pair (4, "Value 4")
-        bst.put(6, "Value 6"); // Insert key-value pair (6, "Value 6")
-        bst.put(9, "Value 9"); // Insert key-value pair (9, "Value 9")
+        // Create a weighted graph
+        WeightedGraph<String> graph = new WeightedGraph<>();
 
-        System.out.println("Value for key 4: " + bst.get(4));
+        // Add vertices to the graph
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
 
-        bst.delete(2);
+        // Add weighted edges to the graph
+        graph.addEdge("A", "B", 5);
+        graph.addEdge("A", "C", 3);
+        graph.addEdge("B", "D", 2);
+        graph.addEdge("C", "D", 1);
+        graph.addEdge("D", "E", 4);
 
-        System.out.println("Size of the tree: " + bst.size());
+        // Create an instance of BreadthFirstSearch and DijkstraSearch
+        BreadthFirstSearch<String> bfsSearch = new BreadthFirstSearch<>(graph);
+        DijkstraSearch<String> dijkstraSearch = new DijkstraSearch<>(graph);
 
-        System.out.println("Keys in ascending order:");
-        for (Integer key : bst.iterator()) {
-            System.out.println(key);
+        // Check if a path exists between vertices A and E using BFS
+        System.out.println("Path from A to E exists (BFS): " + bfsSearch.pathExist("A", "E"));
+
+        // Check if a path exists between vertices A and E using Dijkstra's algorithm
+        System.out.println("Path from A to E exists (Dijkstra): " + dijkstraSearch.pathExist("A", "E"));
+
+        // Find the shortest path between vertices A and E using BFS
+        List<String> bfsShortestPath = bfsSearch.findShortestPath("A", "E");
+
+        // Find the shortest path between vertices A and E using Dijkstra's algorithm
+        List<String> dijkstraShortestPath = dijkstraSearch.findShortestPath("A", "E");
+
+        // Print the shortest path from A to E using BFS if it exists
+        if (bfsShortestPath != null) {
+            System.out.println("Shortest path from A to E (BFS): " + bfsShortestPath);
+        } else {
+            System.out.println("Path from A to E does not exist (BFS)");
+        }
+
+        // Print the shortest path from A to E using Dijkstra's algorithm if it exists
+        if (dijkstraShortestPath != null) {
+            System.out.println("Shortest path from A to E (Dijkstra): " + dijkstraShortestPath);
+        } else {
+            System.out.println("Path from A to E does not exist (Dijkstra)");
         }
     }
-
 }
